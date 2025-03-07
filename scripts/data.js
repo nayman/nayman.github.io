ec875a0defae00c7d0dcc114e1a5032ee0e5050f
nayman <cumatekin@gmail.com>
2025-03-08 02:38:39 +0300

    My First commit
    Personal Portfolio Web site init.

---
+++ scripts/data.js
@@ -0,0 +1,82 @@
+const linkedin = "https://www.linkedin.com/in/cuma-tekin/";
+const github = "https://github.com/nayman/";
+const email = "mailto:cumatekin@gmail.com";
+var today = new Date();
+
+about = [
+  "<br>",
+  "Hey, I'm Cuma Tekin! 👋",
+  "<br>",
+  "A passionate frontend developer from Turkey",
+  "🔭 I’m currently working on [Panates Bilgi Teknolojileri A.Ş.](Business Development Team Lead)",
+  " I’m currently learning react, vue,d3, dax, mquery, graphql",
+  "I love to build things that are useful and can have a positive impact on other people's lives.",
+  "Having said that, I'm always ready to work on quirky projects",
+  "I am always ready to explore new things and collaborate with others.",
+  "Gathering a lot of knowledge and insights.",
+  "Outside of the professional space, I love playing games and casual streaming.",
+  "<br>",
+  "Hope you explore and enjoy this website! 🙏🏻",
+  "<br>",
+];
+
+banner = [
+  "<br>",
+  '<span class = "index">' + String(today) + "</span>",
+  '<span class="index">Cuma Tekin </span>',
+  "<br>",
+"  ██████ ██    ██ ███    ███  █████      ████████ ███████ ██   ██ ██ ███    ██ ",
+" ██      ██    ██ ████  ████ ██   ██        ██    ██      ██  ██  ██ ████   ██ ",
+" ██      ██    ██ ██ ████ ██ ███████        ██    █████   █████   ██ ██ ██  ██ ",
+" ██      ██    ██ ██  ██  ██ ██   ██        ██    ██      ██  ██  ██ ██  ██ ██ ",
+"  ██████  ██████  ██      ██ ██   ██        ██    ███████ ██   ██ ██ ██   ████ ",
+
+  "<br>",
+  '<span class="color2">Welcome to my Portfolio Website..</span>',
+  '<span class="color2">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+  // '<span class="color2"></span>',
+  "<br>",
+];
+
+help = [
+  "<br>",
+  '👦🏻  <span class="command">About</span>          About Me',
+  '🌏  <span class="command">Social</span>         Display social networks',
+  '🏗  <span class="command">Projects</span>       View coding projects',
+  '📧  <span class="command">Email</span>          Reach me through email',
+  '🧹  <span class="command">Clear</span>          Clear the terminal',
+  '❓  <span class="command">Help</span>           Open the help menu',
+  '🕰  <span class="command">History</span>        View command history',
+  '👋  <span class="command">Exit</span>           Close the tab and exit',
+  "<br>",
+];
+
+social = [
+  "<br>",
+  "Enter the name of any of the following or simply click the links...",
+  "<br>",
+  'Linkedin       <a href="' +
+    linkedin +
+    '" target="_blank">linkedin/cuma-tekin' +
+    "</a>",
+  'Github         <a href="' +
+    github +
+    '" target="_blank">github/nayman' +
+    "</a>",
+  "<br>",
+];
+
+projects = [
+  "<br>",
+  '<span class="command">1. WeatherApp</span>',
+  "<br>",
+  "<br>",
+  '<span class="command">2. Mental Health Fitness Tracker',
+  "<br>",
+  "<br>",
+  '<span class="command">3. Portfolio Website',
+  "<br>",
+  "<br>",
+  'Click the following links to access all my projects: <a href = "https://github.com/nayman/" target="_blank">(Github)</a>',
+  "<br>",
+];
